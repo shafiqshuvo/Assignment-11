15f70812f95f21f27bb2ee2c1fcb14fa028ba089
shafiqshuvo <shafiq.shuvo.nstu.ice@gmail.com>
2019-10-16 09:49:12 +0600

    Refrigerator

---
+++ RefrigeratorAppPractice3/Refrigerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    class Refrigerator
+    {
+
+        //private int maxWeight;
+        //private  int numberOfItems;
+        //private int itemWeightPerUnit;
+
+        public double MaxWeight
+        {
+            get; set;
+        }
+        public double NumberOfItem
+        {
+            set; get;
+        }
+        public int WeightPerItem
+        {
+            set; get;
+        }
+        //public int NumberOfItems { get; set; }
+        //public double WeightPerItem { get; set; }
+
+        List<double> NumberOfItems = new List<double>();
+        List<double> WeightPerUnit = new List<double>();
+
+        public void SetNumberOfItem()
+        {
+            NumberOfItems.Add(NumberOfItem);
+        }
+
+        public void SetWeightPerItem()
+        {
+            WeightPerUnit.Add(WeightPerItem);
+        }
+
+        
+
+        public double GetCurrentWeight()
+        {
+            double currentWeight = 0;
+
+
+            for (int i = 0; i < NumberOfItems.Count; i++)
+            {
+                
+                currentWeight += NumberOfItems[i] * WeightPerUnit[i];
+            } 
+            
+            return currentWeight;
+        }
+
+        public bool isPossible(double currentWeight)
+        {
+            bool isPossible = false;
+
+            if ((this.MaxWeight - currentWeight) >= 0)
+            {
+                isPossible = true;
+            }
+
+            return isPossible;
+        }
+
+        public double GetRemainingtWeight(double currentWeight)
+        {
+            double remainingWeight = 0;            
+            return  remainingWeight = this.MaxWeight - currentWeight;               
+           
+        }
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        Refrigerator refrigerator = new Refrigerator();
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(maxWeightTakeTextBox.Text))
+            {
+                refrigerator.MaxWeight = Convert.ToDouble(maxWeightTakeTextBox.Text);
+                maxWeightTakeTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Max Weight Must be Added.");
+            }
+            
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(itemTextBox.Text) && !String.IsNullOrEmpty(weightTextBox.Text))
+            {
+                refrigerator.NumberOfItem = Convert.ToInt32(itemTextBox.Text);
+                refrigerator.WeightPerItem = Convert.ToInt32(weightTextBox.Text);
+
+                refrigerator.SetNumberOfItem();
+                refrigerator.SetWeightPerItem();
+
+
+                double currentWeight = refrigerator.GetCurrentWeight();
+
+                bool isChecked = refrigerator.isPossible(currentWeight);
+
+                if (isChecked)
+                {
+                    double remainingWeight = refrigerator.GetRemainingtWeight(currentWeight);
+                    currentWeightTextBox.Text = currentWeight.ToString();
+                    remainingWeightTextBox.Text = remainingWeight.ToString();
+                }
+
+                else
+                {
+                    MessageBox.Show(" Weights are Maximum, item can't be added ");
+                }
+
+                itemTextBox.Clear();
+                weightTextBox.Clear();
+            }
+
+            else
+            {
+                MessageBox.Show("Feild Can't  be Empty ");
+            }
+                       
+        }
+    }
+}
